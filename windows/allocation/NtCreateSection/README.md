daaa2362c90da56e859b79f3d1fb61eb33db532c
xathrya <me@xathrya.id>
2022-03-27 14:44:44 +0700

    allocation with NtCreateSection

---
+++ windows/allocation/NtCreateSection/README.md
@@ -0,0 +1,25 @@
+# Shellcode Loader
+
+Allocate memory for executing shellcode later.
+
+### Overview
+
+Alokasi menggunakan beberapa fungsi internal:
+- `NtCreateSection`
+- `NtMapViewOfSection`
+
+Teknik ini akan membuat sebuah section baru saat runtime yang dapat menampung shellcode. Section haruslah executable saat eksekusi dan telah dipetakan ke process.
+
+```c++
+NTSTATUS NtCreateSection (PHANDLE SectionHandle, ULONG DesiredAccess, POBJECT_ATTRIBUTES ObjectAttributes, PLARGE_INTEGER MaximumSize, ULONG PageAttributess, ULONG SectionAttributes, HANDLE FileHandle);
+
+NTSTATUS NtMapViewOfSection (HANDLE SectionHandle, HANDLE ProcessHandle, PVOID *BaseAddress, ULONG ZeroBits, ULONG CommitSize, PLARGE_INTEGER SectionOffset, PULONG ViewSize, DWORD InheritDisposition, ULONG AllocationType, ULONG Protect);
+
+NTSTATUS NtUnmapViewOfSection (HANDLE ProcessHandle, PVOID BaseAddress);
+```
+
+### Reference 
+
+- [NTInternals NtCreateSection](http://undocumented.ntinternals.net/index.html?page=UserMode%2FUndocumented%20Functions%2FNT%20Objects%2FSection%2FNtCreateSection.html)
+- [NtInternals NtMapViewOfSection](http://undocumented.ntinternals.net/UserMode/Undocumented%20Functions/NT%20Objects/Section/NtMapViewOfSection.html)
+- [NtInternals NtUnmapViewOfSection](http://undocumented.ntinternals.net/UserMode/Undocumented%20Functions/NT%20Objects/Section/NtUnmapViewOfSection.html)
---
+++ windows/allocation/NtCreateSection/c++/code.cpp
@@ -0,0 +1,122 @@
+/*
+    Shellcode Loader
+    Archive of Reversing.ID
+
+    Allocating new page and write shellcode into it.
+
+Compile:
+    $ cl.exe /nologo /Ox /MT /W0 /GS- /DNDEBUG /Tccode.cpp
+
+Technique:
+    - allocation: NtCreateSection + NtMapViewOfSection
+    - permission: 
+    - execution:  CreateThread
+*/
+
+#include <windows.h>
+#include <stdint.h>
+
+/* ========= some definition ========= */
+#define STATUS_SUCCESS       ((NTSTATUS)0x00000000L)
+#define NT_SUCCESS(Status)   ((NTSTATUS)(Status) == STATUS_SUCCESS)
+#define GetCurrentProcess()  ((HANDLE)(LONG_PTR) -1)
+
+typedef struct _UNICODE_STRING {
+	USHORT Length;
+	USHORT MaximumLength;
+	PWSTR Buffer;
+} UNICODE_STRING, *PUNICODE_STRING;
+
+typedef struct _OBJECT_ATTRIBUTES {
+	ULONG   Length;
+	HANDLE  RootDirectory;
+	PUNICODE_STRING ObjectName;
+	ULONG   Attributes;
+	PVOID   SecurityDescriptor;
+	PVOID   SecurityQualityOfService;
+} OBJECT_ATTRIBUTES, *POBJECT_ATTRIBUTES;
+
+
+/* ========= function signatures ========= */
+typedef NTSTATUS NTAPI NtCreateSection_t(
+    PHANDLE SectionHandle,
+    ULONG   DesiredAccess,
+    POBJECT_ATTRIBUTES ObjectAttributes,
+    PLARGE_INTEGER MaximumSize,
+    ULONG   PageAttributess,
+    ULONG   SectionAttributes,
+    HANDLE  FileHandle
+);
+typedef NtCreateSection_t FAR * pNtCreateSection;
+
+typedef NTSTATUS NTAPI NtMapViewOfSection_t (
+    HANDLE  SectionHandle,
+    HANDLE  ProcessHandle,
+    PVOID  *BaseAddress,
+    ULONG_PTR ZeroBits, 
+    SIZE_T  CommitSize,
+    PLARGE_INTEGER SectionOffset,
+    PSIZE_T ViewSize,
+    DWORD   InheritDisposition,
+    ULONG   AllocationType,
+    ULONG   Protect
+);
+typedef NtMapViewOfSection_t FAR * pNtMapViewOfSection;
+
+typedef NTSTATUS NTAPI NtUnmapViewOfSection_t (
+    HANDLE  ProcessHandle,
+    PVOID   BaseAddress
+);
+typedef NtUnmapViewOfSection_t FAR * pNtUnmapViewOfSection;
+
+
+int main ()
+{
+    void *  runtime = NULL;
+    BOOL    retval;
+    HANDLE  h_thread;
+    DWORD   old_protect = 0;
+
+    // shellcode storage in stack
+    uint8_t     payload []  = { 0x90, 0x90, 0xCC, 0xC3 };
+    uint32_t    payload_len = 4;
+
+    NTSTATUS    status;
+    HMODULE     ntdll;
+    HANDLE      h_section;
+    SIZE_T      size = 0x1000;
+
+    LARGE_INTEGER   capacity;
+
+    // function pointer to internal API
+    pNtCreateSection        NtCreateSection;
+    pNtMapViewOfSection     NtMapViewOfSection;
+    pNtUnmapViewOfSection   NtUnmapViewOfSection;
+
+    // resolve all functions
+    ntdll = GetModuleHandle("ntdll.dll");
+    NtCreateSection         =   (pNtCreateSection)    GetProcAddress(ntdll, "NtCreateSection");
+    NtMapViewOfSection      =  (pNtMapViewOfSection)  GetProcAddress(ntdll, "NtMapViewOfSection");
+    NtUnmapViewOfSection    = (pNtUnmapViewOfSection) GetProcAddress(ntdll, "NtUnmapViewOfSection");
+    
+    // allocate memory buffer for payload as READ-WRITE (no executable)
+    capacity.HighPart = 0;
+    capacity.LowPart  = 0x1000;
+
+    // allocate memory buffer for payload as READ-WRITE-EXECUTE (RWX)
+    // note: need research to makt it RW at first and then change it to RX
+    NtCreateSection (&h_section, SECTION_MAP_READ | SECTION_MAP_WRITE | SECTION_MAP_EXECUTE, NULL, &capacity, PAGE_EXECUTE_READWRITE, SEC_COMMIT, NULL);
+    NtMapViewOfSection (h_section, GetCurrentProcess(), &runtime, NULL, NULL, NULL, &size, 1, 0, PAGE_EXECUTE_READWRITE);
+
+    // copy payload to the buffer
+    RtlMoveMemory (runtime, payload, payload_len);
+
+    // execute as new thread
+    h_thread = CreateThread (0, 0, (LPTHREAD_START_ROUTINE) runtime, 0, 0, 0);
+    WaitForSingleObject (h_thread, -1);
+
+    // deallocate the space
+    NtUnmapViewOfSection (GetCurrentProcess(), &runtime);
+
+    return 0;
+}
