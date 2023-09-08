2a8a47f6b0c25e116896ba238f2a8bac3a16ea09
Satria ady Pradana <xathrya@reversing.id>
2023-09-08 10:34:48 +0700

    writing with Atbash (substitution)

---
+++ windows/writing/substitution/atbash/README.md
@@ -0,0 +1,7 @@
+# Shellcode Loader
+
+Transforming the shellcode by byte substitution.
+
+### Overview
+
+Menggunakan enkripsi klasik Atbash untuk mengganti byte berdasarkan pemetaan alfabet. Rumus matematis untuk enkripsi dan dekripsi (dalam 256 kemungkinan alfabet) adalah: `255-x`
---
+++ windows/writing/substitution/atbash/c++/generator.cpp
@@ -0,0 +1,61 @@
+/*
+    Generator
+    substitution with Affine Cipher
+
+Compile:
+    $ cl.exe /nologo /Ox /MT /W0 /GS- /DNDEBUG /Tcgenerate.cpp 
+*/
+
+#include <windows.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void substitution (uint8_t * dst, uint8_t * src, size_t size)
+{
+    for (size_t idx=0; idx<size; idx++) {
+        dst[idx] = 255 - src[idx];
+    }
+}
+
+
+int main()
+{
+    HANDLE  f;
+    SIZE_T  payload_len;
+    DWORD   nread;
+
+    uint8_t  *  payload;
+    uint32_t    idx;
+
+
+    // open existing file
+    f = CreateFile ("shellcode.bin", GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+    // query the size and create enough space in heap
+    payload_len = GetFileSize (f, NULL);
+    payload = (uint8_t*) HeapAlloc (GetProcessHeap(), HEAP_ZERO_MEMORY, payload_len);
+
+    // read the shellcode
+    ReadFile(f, payload, payload_len, &nread, NULL);
+
+    substitution(payload, payload, payload_len);
+
+    // print
+    printf("{");
+    for (idx = 0; idx < payload_len; idx++)
+    {
+        if (idx % 16 == 0)
+            printf("\n  ");
+        
+        printf("0x%02x, ", payload[idx]);
+    }
+    printf("\n}\n");
+
+    printf ("Length: %lld\n", payload_len);
+
+    // destroy heap
+    HeapFree (GetProcessHeap(), 0, payload);
+
+    return 0;
+}
